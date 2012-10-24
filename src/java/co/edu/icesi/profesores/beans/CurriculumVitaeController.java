a6188971f1e607da3f436bb3fe2a1f1ed7c226a6
David Andres Manzano Herrera <damh24@gmail.com>
2012-10-24 14:14:42 -0500

    Sorting web presentation

--- src/java/co/edu/icesi/profesores/beans/CurriculumVitaeController.java
+++ src/java/co/edu/icesi/profesores/beans/CurriculumVitaeController.java
@@ -23,7 +23,6 @@ import javax.swing.text.html.parser.Entity;
 public class CurriculumVitaeController implements Serializable {
 
     private StdPerson person;
-    private List<M4ccbCvArtPub> m4ccbCvArtPub;
     private M4scoHHrPos positionperson;
     private M4scoPosition position;
     private StdEmail personmail;
@@ -32,47 +31,56 @@ public class CurriculumVitaeController implements Serializable {
     private List<VrrhProCursoAs> vrrhProCursoAsCurrent;
     private List<VrrhCursosProf> coursesPreHis;
     private List<VrrhProCursoAs> coursesPosHis;
-    /*private DataModel m4ccbCvCapLib;
-     private DataModel m4ccbCvDlloMat;
-     private DataModel m4ccbCvDocTrab;
-     private DataModel m4ccbCvEdicionR;
-     private DataModel m4ccbCvLibro;
-     private DataModel m4ccbCvPrefEpil;*/
-
+    private List<M4ccbCvArtPub> journalArticles;
+    private List<M4ccbCvCapLib> bookChapters;
+    private List<M4ccbCvDlloMat> didacticMaterial;
+    private List<M4ccbCvDocTrab> workingPapers;
+    private List<M4ccbCvEdicionR> revisions;
+    private List<M4ccbCvLibro> books;
+    private List<M4ccbCvPrefEpil> epiloges;
+    private List<M4ccbCvSoftReg> registeredSoftware;
+    private List<M4ccbCvTrabTecn> technicalWorks;
+    private List<StdHrLangTrans> translations;
+    
     /**
      * Creates a new instance of CurriculumVitaeController
      */
     public CurriculumVitaeController() {
-        /* cargar ("94378897");*/
+        loadProfessorData("94378897");
     }
 
     public CurriculumVitaeController(String profesorId) {
-        StdPersonJpaController personController = new StdPersonJpaController(Persistence.createEntityManagerFactory("profesoresPU"));
-        M4ccbCvArtPubJpaController articleController = new M4ccbCvArtPubJpaController(Persistence.createEntityManagerFactory("profesoresPU"));
-        this.person = personController.findStdPersonByStdSsn(profesorId);
-        m4ccbCvArtPub = articleController.findM4ccbCvArtPubfindByStdIdHr(person.getStdPersonPK().getStdIdPerson());
+        loadProfessorData(profesorId);
     }
 
-    private void cargar(String profesorId) {
+    private void loadProfessorData(String profesorId) {
         /**
          * Declaraciones de las controladoras
          */
-        EntityManagerFactory entity = Persistence.createEntityManagerFactory("profesoresPU");
-        StdPersonJpaController personController = new StdPersonJpaController(entity);
-        M4ccbCvArtPubJpaController articleController = new M4ccbCvArtPubJpaController(entity);
-        M4scoHHrPosJpaController personPositionController = new M4scoHHrPosJpaController(entity);
-        M4scoPositionJpaController positionController = new M4scoPositionJpaController(entity);
-        StdEmailJpaController emailController = new StdEmailJpaController(entity);
-        StdHrAcadBackgrJpaController acadController = new StdHrAcadBackgrJpaController(entity);
-        VrrhCursosProfJpaController coursesPreController = new VrrhCursosProfJpaController(entity);
-        VrrhProCursoAsJpaController coursesPosController = new VrrhProCursoAsJpaController(entity);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("profesoresPU");
+        StdPersonJpaController personController = new StdPersonJpaController(emf);        
+        M4scoHHrPosJpaController personPositionController = new M4scoHHrPosJpaController(emf);
+        M4scoPositionJpaController positionController = new M4scoPositionJpaController(emf);
+        StdEmailJpaController emailController = new StdEmailJpaController(emf);
+        StdHrAcadBackgrJpaController acadController = new StdHrAcadBackgrJpaController(emf);
+        VrrhCursosProfJpaController coursesPreController = new VrrhCursosProfJpaController(emf);
+        VrrhProCursoAsJpaController coursesPosController = new VrrhProCursoAsJpaController(emf);
+        M4ccbCvArtPubJpaController journalArticlesController = new M4ccbCvArtPubJpaController(emf);
+        M4ccbCvCapLibJpaController bookChpaterController =new M4ccbCvCapLibJpaController(emf);
+        M4ccbCvDlloMatJpaController didacticMaterialController = new M4ccbCvDlloMatJpaController(emf);
+        M4ccbCvDocTrabJpaController workingPapersController = new M4ccbCvDocTrabJpaController(emf);
+        M4ccbCvEdicionRJpaController revisionsController= new M4ccbCvEdicionRJpaController(emf);
+        M4ccbCvLibroJpaController booksController = new M4ccbCvLibroJpaController(emf);
+        M4ccbCvPrefEpilJpaController epilogesController = new M4ccbCvPrefEpilJpaController(emf);
+        M4ccbCvSoftRegJpaController registeredSoftwareController = new M4ccbCvSoftRegJpaController(emf);
+        M4ccbCvTrabTecnJpaController technicalWorksController = new M4ccbCvTrabTecnJpaController(emf);
+        StdHrLangTransJpaController translationsController = new StdHrLangTransJpaController(emf);
         /*Fin declaraciones controladoras*/
 
         this.person = personController.findStdPersonByStdSsn(profesorId);
         String stdIdPerson = person.getStdPersonPK().getStdIdPerson();
         this.positionperson = personPositionController.findPersonPositionByStdSsn(stdIdPerson);
-        this.position = positionController.findPositionById(positionperson.getScoIdPosition());
-        this.m4ccbCvArtPub = articleController.findM4ccbCvArtPubfindByStdIdHr(stdIdPerson);
+        this.position = positionController.findPositionById(positionperson.getScoIdPosition());        
         this.personmail = emailController.findEmailPreferidoByStdHrId(stdIdPerson);
         this.stdHrAcadBackgr = acadController.findEmailPreferidoByStdHrId(stdIdPerson);
 
@@ -80,26 +88,25 @@ public class CurriculumVitaeController implements Serializable {
         this.vrrhProCursoAsCurrent = coursesPosController.findVrrhCursosProfByProfesorPeriodCurrent(profesorId);
         this.coursesPreHis = coursesPreController.findVrrhCursosProfByProfesorPeriodHist(profesorId);
         this.coursesPosHis = coursesPosController.findVrrhCursosProfByProfesorPeriodHist(profesorId);
+        
+        this.bookChapters=bookChpaterController.findM4ccbCvCapLibByStdIdHr(stdIdPerson);
+        this.books=booksController.findM4ccbCvLibroByStdIdHr(stdIdPerson);
+        this.didacticMaterial=didacticMaterialController.findM4ccbCvDlloMatByStdIdHr(stdIdPerson);
+        this.epiloges=epilogesController.findM4ccbCvPrefEpilByStdIdHr(stdIdPerson);
+        this.journalArticles=journalArticlesController.findM4ccbCvArtPubfindByStdIdHr(stdIdPerson);
+        this.registeredSoftware=registeredSoftwareController.findM4ccbCvSoftRegByStdIdHr(stdIdPerson);
+        this.revisions=revisionsController.findM4ccbCvEdicionRByStdIdHr(stdIdPerson);
+        this.technicalWorks=technicalWorksController.findM4ccbCvTrabTecnByStdIdHr(stdIdPerson);
+        this.translations=translationsController.findStdHrLangTransByStdIdHr(stdIdPerson);
+        this.workingPapers=workingPapersController.findM4ccbCvDocTrabByStdIdHr(stdIdPerson);
     }
 
     public StdPerson getPerson() {
-        if (this.person == null) {
-            cargar("94378897");
-        }
         return person;
     }
 
     public void setPerson(StdPerson person) {
         this.person = person;
-    }
-
-    public List<M4ccbCvArtPub> getM4ccbCvArtPub() {
-
-        return m4ccbCvArtPub;
-    }
-
-    public void setM4ccbCvArtPub(List<M4ccbCvArtPub> m4ccbCvArtPub) {
-        this.m4ccbCvArtPub = m4ccbCvArtPub;
     }
 
     public M4scoHHrPos getPositionperson() {
@@ -165,6 +172,85 @@ public class CurriculumVitaeController implements Serializable {
     public void setCoursesPosHis(List<VrrhProCursoAs> coursesPosHis) {
         this.coursesPosHis = coursesPosHis;
     }
-    
+
+    public List<M4ccbCvArtPub> getJournalArticles() {
+        return journalArticles;
+    }
+
+    public void setJournalArticles(List<M4ccbCvArtPub> journalArticles) {
+        this.journalArticles = journalArticles;
+    }
+
+    public List<M4ccbCvCapLib> getBookChapters() {
+        return bookChapters;
+    }
+
+    public void setBookChapters(List<M4ccbCvCapLib> bookChapters) {
+        this.bookChapters = bookChapters;
+    }
+
+    public List<M4ccbCvDlloMat> getDidacticMaterial() {
+        return didacticMaterial;
+    }
+
+    public void setDidacticMaterial(List<M4ccbCvDlloMat> didacticMaterial) {
+        this.didacticMaterial = didacticMaterial;
+    }
+
+    public List<M4ccbCvDocTrab> getWorkingPapers() {
+        return workingPapers;
+    }
+
+    public void setWorkingPapers(List<M4ccbCvDocTrab> workingPapers) {
+        this.workingPapers = workingPapers;
+    }
+
+    public List<M4ccbCvEdicionR> getRevisions() {
+        return revisions;
+    }
+
+    public void setRevisions(List<M4ccbCvEdicionR> revisions) {
+        this.revisions = revisions;
+    }
+
+    public List<M4ccbCvLibro> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<M4ccbCvLibro> books) {
+        this.books = books;
+    }
+
+    public List<M4ccbCvPrefEpil> getEpiloges() {
+        return epiloges;
+    }
+
+    public void setEpiloges(List<M4ccbCvPrefEpil> epiloges) {
+        this.epiloges = epiloges;
+    }
+
+    public List<M4ccbCvSoftReg> getRegisteredSoftware() {
+        return registeredSoftware;
+    }
+
+    public void setRegisteredSoftware(List<M4ccbCvSoftReg> registeredSoftware) {
+        this.registeredSoftware = registeredSoftware;
+    }
+
+    public List<M4ccbCvTrabTecn> getTechnicalWorks() {
+        return technicalWorks;
+    }
+
+    public void setTechnicalWorks(List<M4ccbCvTrabTecn> technicalWorks) {
+        this.technicalWorks = technicalWorks;
+    }
+
+    public List<StdHrLangTrans> getTranslations() {
+        return translations;
+    }
+
+    public void setTranslations(List<StdHrLangTrans> translations) {
+        this.translations = translations;
+    }
     
 }
