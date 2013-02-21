927fb588938485e09122f6994ac1bbb7c84e4094
David Andres Manzano Herrera <damh24@gmail.com>
2013-02-21 10:16:35 -0500

    Implement UI for theCV page's courses section.

--- web/resources/js/main.js
+++ web/resources/js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function(){
+    var accordionIcons = {
+      header: "ui-icon-circle-plus",
+      activeHeader: "ui-icon-circle-minus"
+    };
+    $(".collapsiblePanel").accordion({
+        active: false,
+        collapsible: true,        
+        icons:accordionIcons
+    });
+});  
 
